a78a133094610951ef137f727a3f3a3595fa2c3b
Max Wofford <max.speed.wofford@gmail.com>
2014-09-18 12:15:15 -0600

    Added game_state to initialization

--- game.rb
+++ game.rb
@@ -1,13 +1,21 @@
 require './name.rb'
+require 'json'
 def play()
   
 end
+
 def init()
-  player_name = Name.new().instance_variable_get(:@name)
-  puts player_name
+  # INIT: THE GAME
+  data = JSON.parse(IO.read("game_state.json"))
+  game = data['game_state'].map { |rd| Game_state.new(rd['phone'], rd['addr']) }
+  # Player Attributes
+  game["game_state"]["player"]["attributes"]["name"] = Name.new().instance_variable_get(:@name)
 end
+
 def start()
+  prints '(Press ENTER)'
   $stdin.gets.chomp
+
   init()
   play()
 end
